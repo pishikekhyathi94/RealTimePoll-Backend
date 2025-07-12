c8746691d57630822a155b844f53eb15da1ac7a0
Dutt Nimmagadda <duttnimmagadda2003@gmail.com>
2025-07-12 02:19:09 -0500

    changes made to register.js

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -13,18 +13,7 @@ module.exports = (sequelize, Sequelize) => {
     email: {
       type: Sequelize.STRING,
       allowNull: false,
-      validate: {
-        isEmail: true,
-        isValidForType(value) {
-          if (this.userType === "student" && !value.endsWith("@std.edu")) {
-            throw new Error("Student email must end with @stu.edu");
-          }
-          if (this.userType === "professor" && !value.endsWith("@proff.edu")) {
-            throw new Error("Professor email must end with @proff.edu");
-          }
-        },
-      },
-    },
+          },
     password: {
       type: Sequelize.BLOB,
       allowNull: false,
@@ -33,6 +22,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.BLOB,
       allowNull: false,
     },
+    
   });
 
   return User;
