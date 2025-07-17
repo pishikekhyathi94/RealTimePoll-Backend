cc45861d394b77bb160dceb8ab5db766c84256ad
Sirichandana0110 <Sirichandanaupputuri@gmail.com>
2025-07-17 15:52:25 -0500

    final

--- app/routes/class.routes.js
+++ app/routes/class.routes.js
@@ -2,11 +2,14 @@ module.exports = app => {
   const classes = require("../controllers/class.controller.js");
   var router = require("express").Router();
 
-  // Creating a new class
+  // Create a new class
   router.post("/classes", classes.create);
 
-  // Get all classes by using userid
-  router.get("/classes/userid", classes.findAllForUser);
+  // Get all classes for a specific user
+  router.get("/classes/user/:userId", classes.findAllForUser);
+
+  // Get a single class by id
+  router.get("/classes/:id", classes.findOne);
 
   app.use("/Realtimepoll", router);
 };
