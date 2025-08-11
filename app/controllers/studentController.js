99a3862e7b987a6a18a52c10c77231e0402c9a11
Dutt Nimmagadda <duttnimmagadda2003@gmail.com>
2025-08-11 01:11:29 -0500

    update finish quiz

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -52,20 +52,77 @@ exports.submitQuiz = (req, res) => {
 };
 
 
-exports.finishQuiz = (req, res) => {
-  if (!req.body.quizId || !req.body.userId) {
-    return res.status(400).send({ message: "quizId and userId are required!" });
-  }
-
-  const finish = {
-    quizId: req.body.quizId,
-    userId: req.body.userId,
-  };
-  db.finishQuiz
-    .create(finish)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => {
-      console.error("Error finishing quiz:", err);
-      return res.status(500).send({ message: err.message });
+exports.finishQuiz = async (req, res) => {
+  try {
+    if (!req.body.quizId || !req.body.userId) {
+      return res
+        .status(400)
+        .send({ message: "quizId and userId are required!" });
+    }
+    let userId = req.body.userId;
+    let quizId = req.body.quizId;
+    let student_quiz_finished = await db.finishQuiz.findOne({
+      where: { userId: req.body.userId, quizId: req.body.quizId },
     });
+    if (student_quiz_finished) {
+      return res.status(400).json("Quiz already attempted");
+    }
+    const quiz_student = await db.quiz.findOne({
+      where: { id: quizId },
+    });
+    const startTime = quiz_student.start_time;
+    if (startTime > new Date()) {
+      return res
+        .status(400)
+        .json({ message: `Quiz will start at ${startTime}` });
+    }
+
+    await db.finishQuiz.create({
+      userId: userId,
+      quizId: quizId,
+    });
+    const quizdetails = await db.quiz.findOne({
+      where: { id: quizId },
+      include: [
+        {
+          model: db.question,
+          as: "question",
+        },
+      ],
+    });
+
+    if (!quizdetails) {
+      return res.status(404).json({ message: "Quiz does not exists." });
+    }
+
+    const now = new Date();
+    const completedSeconds = Math.floor((now - startTime) / 1000);
+    let cumulativeTime = 0;
+    let current_question = null;
+    for (const question of quizdetails.question) {
+      cumulativeTime += question.timer;
+      if (completedSeconds < cumulativeTime) {
+        current_question = question;
+        break;
+      }
+    }
+    if (!current_question) {
+      return res
+        .status(400)
+        .json({ message: "Quiz closed. Cannot take now", is_closed: true });
+    }
+
+    const options = await db.option.findAll({
+      where: { questionId: current_question.id },
+    });
+
+    return res.status(200).json({
+      question: current_question,
+      options: options,
+      timeLeftForCurrentQuestion: cumulativeTime - completedSeconds,
+    });
+  } catch (error) {
+    console.error("Error starting quiz:", error);
+    return res.status(500).send({ message: "Server error." });
+  }
 };
